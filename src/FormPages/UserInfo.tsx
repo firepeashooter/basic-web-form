124fa206c1f3a75fcc3f5e5bc18e919dcf94017b
Ben Braun <benjamin.braun2003@gmail.com>
2026-07-20 12:22:17 -0500

    feat: add User Information section to form

---
+++ src/FormPages/UserInfo.tsx
@@ -0,0 +1,34 @@
+import TextInput from "../Components/TextInput"
+
+
+function UserInfo() {
+
+	return (
+		<div className="flex flex-col justify-center gap-5">
+
+			<h1 className="text-xl font-bold">User Information</h1>
+
+			<div className="flex flex-col gap-3 md:grid md:grid-cols-2 md:grid-rows-2">
+
+				<TextInput id="firstname" label="First Name" required />
+				<TextInput id="lastname" label="Last Name" required />
+				<TextInput id="email" label="Email" required />
+				<TextInput id="phone" label="Phone Number" />
+			</div>
+
+			<div className="flex justify-end">
+				<button
+					type="button"
+					className="rounded-xl bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all hover:bg-blue-500 active:scale-[0.99] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 disabled:opacity-50 disabled:pointer-events-none"
+				>
+					Next
+				</button>
+			</div>
+
+
+
+		</div>
+	)
+}
+
+export default UserInfo
